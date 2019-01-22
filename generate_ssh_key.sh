48abc1745cc034a2e1c2aee5f0ed5659099f5e9f
nikcbg <43785382+nikcbg@users.noreply.github.com>
2019-01-22 08:12:57 +0200

    Update generate_ssh_key.sh

--- generate_ssh_key.sh
+++ generate_ssh_key.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-
-# Make a directory to contain the key
-mkdir -p test/assets
+# Go to test directory and run the script below to generate SSH key.
+cd test
 
 # Generate a 4096 bit RSA key with a blank passphrase in the directory
 ssh-keygen \
